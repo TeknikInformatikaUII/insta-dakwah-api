6a92139180f6eb8bd4a9604d31f7b07e1181ba8c
nmfzone <nabilftd@gmail.com>
2016-11-22 02:09:59 +0700

    Added config files

--- config/auth.php
+++ config/auth.php
@@ -64,11 +64,6 @@ return [
             'driver' => 'eloquent',
             'model' => App\User::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
 ];
