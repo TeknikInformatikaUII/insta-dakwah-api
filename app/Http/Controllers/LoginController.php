2dd3d2a201d0ec0e4d331241e8d1890c7247fb63
nmfzone <nabilftd@gmail.com>
2016-11-22 02:13:40 +0700

    Added login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = [
+            'username' => $request->username,
+            'password' => $request->password,
+        ];
+
+        return $this->access('password', $credentials);
+    }
+
+    /**
+     * Handle a refresh token request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function refreshToken(Request $request)
+    {
+        $crypt = app()->make('encrypter');
+        $request = app()->make('request');
+
+        return $this->access('refresh_token', [
+            'refresh_token' => $crypt->decrypt($request->cookie('refreshToken'))
+        ]);
+    }
+
+    /**
+     * Send request to the laravel passport.
+     *
+     * @param  string  $grantType
+     * @param  array  $data
+     * @return \Illuminate\Http\Response
+     */
+    private function access($grantType, array $data = [])
+    {
+        try {
+            $config = app()->make('config');
+
+            $data = array_merge([
+                'client_id'     => $config->get('secrets.client_id'),
+                'client_secret' => $config->get('secrets.client_secret'),
+                'grant_type'    => $grantType
+            ], $data);
+
+            $http = new Client();
+
+            $guzzleResponse = $http->post($config->get('app.url').'/oauth/token', [
+                'form_params' => $data
+            ]);
+        } catch(\GuzzleHttp\Exception\BadResponseException $e) {
+            $guzzleResponse = $e->getResponse();
+        }
+
+        $response = json_decode($guzzleResponse->getBody());
+
+        if (property_exists($response, "access_token")) {
+            $cookie = app()->make('cookie');
+            $crypt  = app()->make('encrypter');
+
+            $encryptedToken = $crypt->encrypt($response->refresh_token);
+
+            // Set the refresh token as an encrypted HttpOnly cookie
+            $cookie->queue('refreshToken',
+                $encryptedToken,
+                604800, // expiration, should be moved to a config file
+                null,
+                null,
+                false,
+                true // HttpOnly
+            );
+
+            $this->accessToken = $response->access_token;
+
+            $response = [
+                'accessToken'            => $response->access_token,
+                'tokenType'              => $response->token_type,
+                'accessTokenExpiration'  => $response->expires_in
+            ];
+        }
+
+        $response = response()->json($response);
+        $response->setStatusCode($guzzleResponse->getStatusCode());
+
+        $headers = $guzzleResponse->getHeaders();
+        foreach($headers as $headerType => $headerValue) {
+            $response->header($headerType, $headerValue);
+        }
+
+        return $response;
+    }
+
+}
