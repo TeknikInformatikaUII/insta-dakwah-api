cc38b9e80443928bc86c1d731a4e16f19d59cb37
nmfzone <nabilftd@gmail.com>
2016-11-23 11:45:57 +0700

    Added user_followers table

---
+++ database/migrations/2016_11_22_152241_create_user_followers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_followers', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('follower_id');
+            $table->timestamps();
+            $table->index(['user_id', 'follower_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_followers');
+    }
+}
